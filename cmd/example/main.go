fb807d18f7e39d00ded87c8cc10875857bb3fc66
xastia <122154736+xastia@users.noreply.github.com>
2024-03-21 15:45:20 +0300

    Update main.go

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,25 +3,74 @@ package main
 import (
 	"flag"
 	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	lab2 "github.com/EugeneSemivolos/architecture-lab-2"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	inputFile       = flag.String("f", "", "File with expression to compute")
+	outputFile      = flag.String("o", "", "File to store computed expression")
 )
 
 func main() {
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	if *inputExpression == "" && *inputFile == "" {
+		log.Fatal("no expression provided. use -e \"{expression}\" or -f {file with expression}")
+	}
 
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+	if *inputExpression != "" && *inputFile != "" {
+		log.Fatal("flags -e and -f can't both be used")
+	}
+
+	var reader io.Reader
+
+	if *inputExpression != "" {
+		reader = strings.NewReader(*inputExpression)
+	} else {
+		file, err := os.Open(*inputFile)
+		if err != nil {
+			log.Fatal("no such file")
+		}
+		reader = file
+		defer file.Close()
+	}
+
+	var writer io.Writer
+
+	if *outputFile != "" {
+		file, err := os.Create(*outputFile)
+		if err != nil {
+			log.Fatal("something went wrong while creating file")
+		}
+
+		writer = file
+		defer file.Close()
+	} else {
+		writer = &Writer{}
+	}
+
+	handler := &lab2.ComputeHandler{
+		Input:  reader,
+		Output: writer,
+	}
+
+	err := handler.Compute()
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+}
+
+type Writer struct{}
+
+func (w *Writer) Write(data []byte) (n int, err error) {
+	fmt.Println(string(data))
+	return len(data), nil
 }
