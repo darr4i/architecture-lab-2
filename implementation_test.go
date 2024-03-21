955b65fca1ad2ab8f47b258c1d07c35dfaef5220
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2024-03-21 14:43:23 +0200

    Update implementation_test.go

--- implementation_test.go
+++ implementation_test.go
@@ -4,20 +4,43 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "gopkg.in/check.v1"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+
+
+func (s *MySuite) TestCalculatePrefix(c *C) {
+	result, err := CalculatePrefix("+ 5 * - 4 2 3")
+	c.Assert(result, Equals, "11")
+  
+	result, err = CalculatePrefix("- * + 4 6 - 3 + * 4 6 * 3 5 - * 2 4 * 3 7")
+	c.Assert(result, Equals, "-347")
+  
+	result, err = CalculatePrefix("")
+	c.Assert(err, ErrorMatches, "Unexpected symbols or Extra spaces")
+  
+	result, err = CalculatePrefix("+ a b")
+	c.Assert(err, ErrorMatches, "Unexpected symbols or Extra spaces")
+  
+	result, err = CalculatePrefix("- + 5 5")
+	c.Assert(err, ErrorMatches, "Incorrect ratio of numbers and operators")
+  
+	result, err = CalculatePrefix("+ 5 0 2 4 + -")
+	c.Assert(err, ErrorMatches, "Not a prefix notation order")
+  }
+
+  func ExampleCalculatePrefix() {
+	res, err := CalculatePrefix("+ 5 * - 4 2 3")
+	if err != nil {
+	  panic(err)
+	} else {
+	  fmt.Println(res)
 	}
-}
-
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
-
-	// Output:
-	// 2 2 +
-}
+	// Output: 11
+  }
