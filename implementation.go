dd299af856f8f0f85a13abe8b604c2ffefaa88d3
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2024-03-21 16:14:22 +0200

    Update implementation.go

--- implementation.go
+++ implementation.go
@@ -3,107 +3,95 @@ package lab2
 import (
 	"fmt"
 	"math"
-	"strconv"
 	"strings"
-	"unicode"
+	"strconv"
+	"regexp"
 )
 
-const (
-	operators string = "+-*/^"
-)
+type Stack []string
 
-func CalculatePrefix(input string) (int, error) {
-	var stack Stack
-	var num int
-	var err error
-
-	input = strings.Trim(input, " ")
-
-	for i := len(input) - 1; i >= 0; i-- {
-		clearSpaces(input, &i)
-
-		if unicode.IsDigit(rune(input[i])) {
-			num, i, err = getNumber(input, i)
-			if err != nil {
-				return -1, err
-			}
-			stack.Push(num)
-		} else {
-			if !strings.Contains(operators, string(input[i])) {
-				return -1, fmt.Errorf("Error. Problem with opertator")
-			}
-			if len(stack) < 2 {
-				return -1, fmt.Errorf("Error. Missing arguments or many operators")
-			}
-			o1, _ := stack.Pop()
-			o2, _ := stack.Pop()
-			switch input[i] {
-			case '+':
-				stack.Push(o1 + o2)
-			case '-':
-				stack.Push(o1 - o2)
-			case '*':
-				stack.Push(o1 * o2)
-			case '/':
-				if o2 == 0 {
-					return -1, fmt.Errorf("Error. Divide by zero")
-				}
-				stack.Push(o1 / o2)
-			case '^':
-				stack.Push(int(math.Pow(float64(o1), float64(o2))))
-			}
-		}
-	}
-	if stack.Length() != 1 {
-		return -1, fmt.Errorf("Error. Missing arguments or many operators")
-	}
-	res, _ := stack.Pop()
-	return res, nil
-}
-
-func clearSpaces(data string, i *int) {
-	for string(data[*i]) == " " {
-		*i--
-	}
-}
-
-func getNumber(data string, i int) (int, int, error) {
-	j := i
-	for unicode.IsDigit(rune(data[j])) {
-		j--
-		if j == -1 {
-			break
-		}
-	}
-	j++
-	result, err := strconv.Atoi(data[j : i+1])
-	if err != nil {
-		return -1, -1, fmt.Errorf("Error. NaN")
-	}
-	return result, j, err
-}
-
-type Stack []int
-
-func (s *Stack) IsEmpty() bool {
-	return len(*s) == 0
-}
-
-func (s *Stack) Length() int {
-	return len(*s)
-}
-
-func (s *Stack) Push(num int) {
-	*s = append(*s, num)
-}
-
-func (s *Stack) Pop() (int, bool) {
-	if s.IsEmpty() {
-		return -1, false
+func (s *Stack) Pop() (string, bool) { //element, err
+	if len(*s) == 0 {
+		return "", true
 	} else {
 		index := len(*s) - 1
 		element := (*s)[index]
 		*s = (*s)[:index]
-		return element, true
+		return element, false
 	}
+}
+
+func evaluate(operator string, a float64, b float64) float64 {
+	if operator == "+" {
+		return a + b
+	} else if operator == "-" {
+		return a - b
+	} else if operator == "*" {
+		return a * b
+	} else if operator == "/" {
+		return a / b
+	} else  {
+		return math.Pow(a, b) // "^"
+	}
+}
+
+func hasFormat(array []string, mask string) bool {
+	for i := 0; i < len(array); i++ {
+		match, _ := regexp.MatchString(mask, array[i])
+		if !match {
+			return false
+		}
+	}
+	return true
+}
+
+func isBalanced(array []string, operators string) bool {
+	numbersCounter := 0 
+	operatorsCounter := 0
+
+	for i := 0; i < len(array); i++ {
+		if strings.Contains(operators, array[i]) {
+			operatorsCounter++
+		} else {
+			numbersCounter++
+		}
+	}
+	return numbersCounter == operatorsCounter + 1
+}
+
+func CalculatePrefix(input string) (string, error) {
+	mask := "^(([0-9]+)|([/+/*///^/-]))$"
+	operators := "+-*/^"
+
+	array := strings.Split(input, " ")
+	var stack Stack
+
+	if !hasFormat(array, mask) {
+		return "", fmt.Errorf("Unexpected symbols or Extra spaces")
+	}
+
+	if !isBalanced(array, operators) {
+		return "", fmt.Errorf("Incorrect ratio of numbers and operators")
+	}
+
+	for i := len(array) - 1; i >= 0; i-- {
+		if !strings.Contains(operators, array[i]) {
+			stack = append(stack, array[i])
+		} else {
+			stringElem1, err1 := stack.Pop()
+			stringElem2, err2 := stack.Pop()
+			if err1 || err2 {
+				return "", fmt.Errorf("Not a prefix notation order")
+			}
+			floatElem1, _ := strconv.ParseFloat(stringElem1, 64)
+			floatElem2, _ := strconv.ParseFloat(stringElem2, 64)
+
+			floatResult := evaluate(array[i], floatElem1, floatElem2)
+			stringResult := strconv.FormatFloat(floatResult, 'f', -1, 64)
+			stack = append(stack, stringResult)
+		}
+	}
+
+	finalResult, _ := stack.Pop()
+	return finalResult, nil
 }
